c24d3aff8773792b14b2171708e69c2d908b5719
Leo Hong <5917188+Low-Earth-Orbit@users.noreply.github.com>
2021-10-10 15:39:36 -0300

    free()

--- antenna.c
+++ antenna.c
@@ -10,7 +10,7 @@
 /*
 	Input file information
 */
-#define INPUT_FILE_PATH "new.txt"
+#define INPUT_FILE_PATH "input.txt"
 #define MAX_NUM_SIGNALS 10000
 #define MAX_NUM_EPOCHES 1000
 #define MAX_NUM_CHAR_LINE 100 // num of char in each record or line
@@ -92,16 +92,16 @@ char* concat(const char* str1, const char* str2)
 
 void printEpochArray(Epoch* epochArray, int numEpoch) {
  	for (int i = 0; i < numEpoch; i++) {
-		printf("======== Epoch %s includes: ========\n", epochArray[i].time);
+		printf("======== Epoch %s contains %i satellites/signals ========\n", epochArray[i].time, epochArray[i].numSat);
 		for (int j = 0; j < epochArray[i].numSat; j++){
-			printf("%s %s %lf %lf %lf\n", epochArray[i].satArray[j].time, epochArray[i].satArray[j].satName, epochArray[i].satArray[j].az, epochArray[i].satArray[j].el, epochArray[i].satArray[j].snr);
+			printf("%s\t%s\t%lf\t%lf\t%lf\n", epochArray[i].satArray[j].time, epochArray[i].satArray[j].satName, epochArray[i].satArray[j].az, epochArray[i].satArray[j].el, epochArray[i].satArray[j].snr);
 		}
 	}
 }
 	
 int main (void) {
-	
-	/* create arrays
+	/*
+		create arrays
 	*/
 	char** timeArray; // A time array storing all unique times
 	Sat* satArray; // A sat array storing all satellite signals
@@ -110,54 +110,66 @@ int main (void) {
 	timeArray = (char**)malloc(MAX_NUM_EPOCHES*sizeof(char*));
 	if (timeArray == NULL) {
 		fprintf(stderr, "malloc() failed for creating timeArray\n");
+		exit(-1);
 	}
 	for (int i = 0; i < MAX_NUM_EPOCHES; i++) {
 		timeArray[i] = (char*)malloc(NUM_CHAR_DATE + NUM_CHAR_TIME + 2);
 		if (timeArray[i] == NULL) {
 			fprintf(stderr, "malloc() failed for creating timeArray element\n");
+			exit(-1);
 		}
 	}
 	
 	satArray = (Sat*)malloc(MAX_NUM_SIGNALS*sizeof(Sat));
 	if (satArray == NULL) {
 		fprintf(stderr, "malloc() failed for creating satArray\n");
+		exit(-1);
 	}
 	
 	epochArray = (Epoch*)malloc(MAX_NUM_EPOCHES*sizeof(Epoch));
+	if (epochArray == NULL) {
+		fprintf(stderr, "malloc() failed for creating epochArray\n");
+		exit(-1);
+	}
 	
+	/*
+		array indexes
+	*/
 	int satArrayIndex = 0;
 	int timeArrayIndex = 0; // epoch array index is the same, or number of epoches
 	
-
-	/* opening file for reading */
+	/*
+		open file for reading
+	*/
 	FILE *fp = NULL;
 	fp = fopen(INPUT_FILE_PATH, "r");
 	if(fp == NULL) {
 		perror("Error opening file\n");
 		return(-1);
 	}
-	
-	char line[MAX_NUM_CHAR_LINE];
-		
-	fgets(line, sizeof(line), fp); // Skip header
-	
+
 	/*
 		File processing to get timeArray and satArray
-	*/
+	*/	
+	char line[MAX_NUM_CHAR_LINE	+1];//+1 in case the max provided does not include the null-terminator
+	fgets(line, sizeof(line), fp); // Skip header
 	while(fgets(line, sizeof(line), fp) != NULL) {
 		char* time1 = (char*)malloc(sizeof(char)*(NUM_CHAR_DATE +1));
 		if (time1 == NULL) {
 			fprintf(stderr, "malloc() failed for creating time1\n");
+			exit(-1);
 		}
 		
 		char* time2 = (char*)malloc(sizeof(char)*(NUM_CHAR_TIME +1));
 		if (time2 == NULL) {
 			fprintf(stderr, "malloc() failed for creating time2\n");
+			exit(-1);
 		}
 		
 		char* satName = (char*)malloc(sizeof(char)*(NUM_CHAR_SAT +1));
 		if (satName == NULL) {
 			fprintf(stderr, "malloc() failed for creating satName\n");
+			exit(-1);
 		}
 		
 		double az;
@@ -166,6 +178,8 @@ int main (void) {
 
 		sscanf(line, "%s %s %s %lf %lf %lf", time1, time2, satName, &az, &el, &snr);
 		char* time = concat(time1, time2);
+		if (time == NULL)
+			exit(-1);//failed to allocate memory in concat() above
 
 		free(time1);
 		free(time2);// time1 and time2 used temporarily
@@ -190,48 +204,62 @@ int main (void) {
 		satArrayIndex++;
 	}
 	
+	/*
+		close file
+	*/
+	fclose(fp);
 	
-	/* for each unique time 
-		1) create an Epoch object;
-		2) load it to epochArray
+	/* 
+		epoch array
+		for each unique time:
+			1) create an Epoch object;
+			2) load it to epochArray
 	*/
  	for (int i = 0; i < timeArrayIndex; i++) {
-		
 		Sat* epochSatArray = (Sat*)malloc(MAX_NUM_SAT_EPOCH*sizeof(Sat));
 		if (epochSatArray == NULL) {
 			fprintf(stderr, "malloc() failed for creating epochSatArray\n");
+			exit(-1);
 		}
 		
 		int epochSatArrayIndex = 0;
 		for (int j = 0; j < satArrayIndex; j++){
 			//printf("%i %s %s\n", satArrayIndex, timeArray[i], satArray[j].time);
-			if (strcmp(timeArray[i], satArray[j].time)==0)
-			{
+			if (strcmp(timeArray[i], satArray[j].time)==0) {
 				epochSatArray[epochSatArrayIndex] = satArray[j];
 				epochSatArrayIndex++;
 			}
-			
 		}
 		Epoch epochObj = createEpoch(timeArray[i], epochSatArray, epochSatArrayIndex); //numSat initially 0
 		epochArray[i] = epochObj;
 	}
-	
+
 	printEpochArray(epochArray, timeArrayIndex);
-	
 	
 	/*
 		free()
+			This notice from valgrind is normal:
+				Conditional jump or move depends on uninitialised value(s)
+			This is because the arrays are not fully allocated; e.g. MAX_NUM_SIGNALS < actual num of signals
+			Could use calloc() instead of malloc()
 	*/
-	
-	/*
- 	for (int i = 0; i < MAX_NUM_EPOCHES; i++) {
+	for (int i = 0; i < MAX_NUM_EPOCHES; i++) {
 		free(timeArray[i]);
 	}
 	free(timeArray);
+	
+ 	for (int i = 0; i < MAX_NUM_SIGNALS; i++) {
+		free(satArray[i].time);
+		free(satArray[i].satName);
+	}
 	free(satArray);
+
+  	for (int i = 0; i < MAX_NUM_EPOCHES; i++)
+		free(epochArray[i].satArray);
+	free(epochArray);
+	
+	/*
+		exit
 	*/
-	
-	fclose(fp); 
-	
 	exit(0);
-}
+} // end of main()
