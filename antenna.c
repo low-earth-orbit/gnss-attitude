d39102e1e9e948f6717d91c844ce85fcb6b76585
Leo Hong <5917188+Low-Earth-Orbit@users.noreply.github.com>
2021-10-26 16:15:14 -0300

    Rework the remaining Axelrad's method and statistics

--- antenna.c
+++ antenna.c
@@ -154,8 +154,8 @@ int main(void)
 
 		for (int j = 0; j < n; j++)
 		{
-			double xyz[3];
 			/* calculate LOS vector from azimuth and elevation*/
+			double xyz[3];
 			ae2xyz(*(*epochArray[i]).epochSatArray[j]->az, *(*epochArray[i]).epochSatArray[j]->el, xyz);
 			//printf("%lf,%lf,%lf\n", xyz[0], xyz[1], xyz[2]);
 
@@ -177,14 +177,12 @@ int main(void)
 
 		/* from xyz solution derive azimuth-elevation solution */
 		xyz2aeSol(*(dunSol->x), *(dunSol->y), *(dunSol->z), dunSol);
-		//printf("%lf,%lf,%lf\n", *(dunSol->x), *(dunSol->y), *(dunSol->z));
 
 		/* print result */
 		printf("%s,%i,%lf,%lf,%lf,%lf,%lf\n", (*epochArray[i]).time, *(*epochArray[i]).numSat, *(dunSol->x), *(dunSol->y), *(dunSol->z), *(dunSol->az), *(dunSol->el));
 
 		dunSolArray[i] = dunSol;
 	}
-	/////////////// OK above
 
 	/*
 		Geometry method -- vector sum of non-weighted line-of-sight (LOS) vectors with geometry adjustment for elevation angle. Duncan's method is biased toward the spherical area where the satellite signals come from. If the antenna's elevation angle is negative, Duncan's method yields a positive elevation angle estimate. This method is designed by the author of the program to address the geometry issue existing in Duncan's method.
@@ -205,8 +203,8 @@ int main(void)
 
 		for (int j = 0; j < n; j++)
 		{
-			double xyz[3];
 			/* calculate LOS vector from azimuth and elevation*/
+			double xyz[3];
 			ae2xyz(*(*epochArray[i]).epochSatArray[j]->az, *(*epochArray[i]).epochSatArray[j]->el, xyz);
 			//printf("%lf,%lf,%lf\n", xyz[0], xyz[1], xyz[2]);
 
@@ -236,10 +234,131 @@ int main(void)
 		/* print result */
 		printf("%s,%i,%lf,%lf,%lf,%lf,%lf\n", (*epochArray[i]).time, *(*epochArray[i]).numSat, *(geoSol->x), *(geoSol->y), *(geoSol->z), *(geoSol->az), *(geoSol->el));
 
+		/* save to array */
 		geoSolArray[i] = geoSol;
 	}
+	//////////////// above OK
+	/*
+		Axelrad's method (Axelrad & Behre, 1999) -- Compared to Duncan's method, this is the proper use of SNR in determining antenna boresight vector. It requires antenna gain mapping (the relationship between off-boresight angle and SNR for the antenna) and adjustment to measured SNR.
+	*/
+	Sol **axelSolArray = malloc(sizeof(Sol *) * epochArrayIndex);
 
-	/////////////////////////////////////////////////////////////////////// deleted
+	//double xyz[epochArrayIndex][MAX_NUM_SAT_EPOCH][3]; // line of sight vectors
+	//double xyzSol[epochArrayIndex][3];
+	//double aeSol[epochArrayIndex][2];
+
+	printf("================== Axelrad's method ==================\nEpoch(GPST),#Sat,X(E),Y(N),Z(U),Az(deg),El(deg)\n");
+	for (long int i = 0; i < epochArrayIndex; i++)
+	{
+
+		/*	
+			Observation equation X*b = cos(a) corresponds to X*c = y below
+			See GNU Scientific Library Reference Manual for more: https://www.gnu.org/software/gsl/doc/html/lls.html
+		*/
+		int n = *(epochArray[i]->numSat); // number of observations in the epoch
+		Sol *axelSol = malloc(sizeof(Sol));
+		axelSol->x = calloc(1, sizeof(double));
+		axelSol->y = calloc(1, sizeof(double));
+		axelSol->z = calloc(1, sizeof(double));
+		axelSol->az = calloc(1, sizeof(double));
+		axelSol->el = calloc(1, sizeof(double));
+
+		/* set up size of matrices for least squares (LS) regression */
+		double chisq;
+		gsl_matrix *X, *cov;
+		gsl_vector *y, *w, *c;
+		X = gsl_matrix_alloc(n, 3);
+		y = gsl_vector_alloc(n);	  // n*1 matrix
+		w = gsl_vector_alloc(n);	  // n diagonal elements of n*n weight matrix
+		c = gsl_vector_alloc(3);	  // coefficients (x, y, z) -> the boresight vector
+		cov = gsl_matrix_alloc(3, 3); // cov = inverse(transpose(X) W X)
+
+		for (int j = 0; j < n; j++)
+		{
+			/* calculate LOS vector from azimuth and elevation*/
+			double xyz[3];
+			ae2xyz(*(*epochArray[i]).epochSatArray[j]->az, *(*epochArray[i]).epochSatArray[j]->el, xyz);
+
+			/* get sigma of cos(a) to be used in weight matrix W */
+			double sigmaSnr = 0.5 + (3 - 0.5) * (*(*epochArray[i]).epochSatArray[j]->snr - 35) / (50 - 35);
+			if (sigmaSnr < 0.5)														  // catch the case that sigma <= 0
+				sigmaSnr = 0.5;														  // set to minumum sigmaSnr
+			double sigma = sigmaSnr / 15.0;											  // uncertainty is a function of SNR. divide sigmaSnr by the coefficient of sigma_cosA
+			double cosA = (*(*epochArray[i]).epochSatArray[j]->snr - 35) / (50 - 35); // find cosA from the mapping function snr = (MAX_SNR-MIN_SNR)*cos(A)+MIN_SNR;
+
+			// Set each observation equation
+			gsl_matrix_set(X, j, 0, xyz[0]); // coefficient c0 = x
+			gsl_matrix_set(X, j, 1, xyz[1]); // coefficient c1 = y
+			gsl_matrix_set(X, j, 2, xyz[2]); // coefficient c2 = z
+			gsl_vector_set(y, j, cosA);
+			gsl_vector_set(w, j, 1.0 / (sigma * sigma));
+		}
+
+		/* run multi-parameter regression */
+		gsl_multifit_linear_workspace *work = gsl_multifit_linear_alloc(n, 3);
+		gsl_multifit_wlinear(X, w, y, c, cov, &chisq, work);
+		gsl_multifit_linear_free(work);
+
+		/* save best fit */
+		*(axelSol->x) = C(0);
+		*(axelSol->y) = C(1);
+		*(axelSol->z) = C(2);
+
+		/* free matrices for LS */
+		gsl_matrix_free(X);
+		gsl_vector_free(y);
+		gsl_vector_free(w);
+		gsl_vector_free(c);
+		gsl_matrix_free(cov);
+
+		/* normalize the resulting vector to get xyz solution*/
+		normalize(axelSol);
+
+		/* from xyz solution derive azimuth-elevation solution */
+		xyz2aeSol(*(axelSol->x), *(axelSol->y), *(axelSol->z), axelSol);
+
+		/* print result */
+		printf("%s,%i,%lf,%lf,%lf,%lf,%lf\n", (*epochArray[i]).time, *(*epochArray[i]).numSat, *(axelSol->x), *(axelSol->y), *(axelSol->z), *(axelSol->az), *(axelSol->el));
+
+		/* save to array */
+		axelSolArray[i] = axelSol;
+	}
+
+	/*
+		Statistics if true antenna boresight (E, N, U) is known 
+	*/
+	if (TRUE_EL >= -90 && TRUE_EL <= 90 && TRUE_AZ <= 360 && TRUE_AZ >= 0)
+	{
+		double rmsDun;
+		double sumDun = 0;
+
+		double rmsGeo;
+		double sumGeo = 0;
+
+		double rmsAxel;
+		double sumAxel = 0;
+
+		for (long int i = 0; i < epochArrayIndex; i++)
+		{
+			double trueAntennaXyz[3];
+			ae2xyz(TRUE_AZ, TRUE_EL, trueAntennaXyz);
+			sumDun += pow(spDist(*dunSolArray[i]->x, *dunSolArray[i]->y, *dunSolArray[i]->z, trueAntennaXyz[0], trueAntennaXyz[1], trueAntennaXyz[2]), 2);
+			sumGeo += pow(spDist(*geoSolArray[i]->x, *geoSolArray[i]->y, *geoSolArray[i]->z, trueAntennaXyz[0], trueAntennaXyz[1], trueAntennaXyz[2]), 2);
+			sumAxel += pow(spDist(*axelSolArray[i]->x, *axelSolArray[i]->y, *axelSolArray[i]->z, trueAntennaXyz[0], trueAntennaXyz[1], trueAntennaXyz[2]), 2);
+		}
+
+		rmsDun = sqrt(sumDun / epochArrayIndex);
+		rmsDun = rad2deg(rmsDun);
+
+		rmsGeo = sqrt(sumGeo / epochArrayIndex);
+		rmsGeo = rad2deg(rmsGeo);
+
+		rmsAxel = sqrt(sumAxel / epochArrayIndex);
+		rmsAxel = rad2deg(rmsAxel);
+
+		printf("================== Statistics ==================\n%li epochs, antenna @ %i deg\nRMS Duncan's method = %lf deg\nRMS Geometry method = %lf deg\nRMS Axelrad's method = %lf deg\n", epochArrayIndex, TRUE_EL, rmsDun, rmsGeo, rmsAxel);
+	}
+
 	/*
 		free() file input 
 	*/
@@ -266,7 +385,7 @@ int main(void)
 	free(epochArray);
 
 	/*
-		free() Duncan's solution
+		free() solutions
 	*/
 	for (long int i = 0; i < epochArrayIndex; i++)
 	{
@@ -279,9 +398,6 @@ int main(void)
 	}
 	free(dunSolArray);
 
-	/*
-		free() geometry solution
-	*/
 	for (long int i = 0; i < epochArrayIndex; i++)
 	{
 		free(geoSolArray[i]->x);
@@ -293,6 +409,16 @@ int main(void)
 	}
 	free(geoSolArray);
 
+	for (long int i = 0; i < epochArrayIndex; i++)
+	{
+		free(axelSolArray[i]->x);
+		free(axelSolArray[i]->y);
+		free(axelSolArray[i]->z);
+		free(axelSolArray[i]->az);
+		free(axelSolArray[i]->el);
+		free(axelSolArray[i]);
+	}
+	free(axelSolArray);
 	/*
 		exit
 	*/
