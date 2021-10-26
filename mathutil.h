d4df0be454da07fe1670fd69d03b61a3ca918442
Leo Hong <5917188+Low-Earth-Orbit@users.noreply.github.com>
2021-10-25 22:18:26 -0300

    Create functions for processing Sol

--- mathutil.h
+++ mathutil.h
@@ -1,14 +1,17 @@
 #ifndef MATHUTIL_H
 #define MATHUTIL_H
-
+#include "struct.h"
 /*
 	Function declarations
 */
-double deg2rad (double deg);
-double rad2deg (double rad);
-void normalizeXyz (double *xyz);
-void ae2xyz (double az_deg, double el_deg, double *xyz);
-void xyz2ae (double x, double y, double z, double* azel);
-double meanAz (double array[], int n);
+double deg2rad(double deg);
+double rad2deg(double rad);
+void normalizeXyz(double *xyz);
+void normalize(Sol *sol);
+void ae2xyz(double az_deg, double el_deg, double *xyz);
+void ae2xyzSol(double az_deg, double el_deg, Sol *sol);
+void xyz2ae(double x, double y, double z, double *azel);
+void xyz2aeSol(double x, double y, double z, Sol *sol);
+double meanAz(double array[], int n);
 double spDist(double a1, double a2, double a3, double b1, double b2, double b3);
 #endif
