cd003e9e4d5ede36904b6afcf5957a51c8ca0911
Leo Hong <5917188+Low-Earth-Orbit@users.noreply.github.com>
2021-10-28 01:28:34 -0300

    change MAX_NUM_EPOCH to 86400

--- config.h
+++ config.h
@@ -2,13 +2,13 @@
 #define CONFIG_H
 
 /* Configuration */
-#define TRUE_EL 90
-#define TRUE_AZ 180		 //If true azimuth and elevation angle of the antenna are known, input here for statistics (in degrees)
+#define TRUE_EL -80
+#define TRUE_AZ 180	   //If true azimuth and elevation angle of the antenna are known, input here for statistics (in degrees)
 #define NUM_EPOCH 1000 // number of epochs to simulate
 #define INPUT_FILE_PATH "input.txt"
 
 /* Usually no need to change*/
-#define MAX_NUM_EPOCH 864000  // up to 24h data @ 1hz rate
+#define MAX_NUM_EPOCH 86400	  // up to 24h data @ 1hz rate
 #define MAX_NUM_SAT_EPOCH 100 // maximum number of satellites visible in an epoch
 #define MAX_NUM_SIGNAL (MAX_NUM_EPOCH * MAX_NUM_SAT_EPOCH)
 #define MAX_NUM_CHAR_LINE 100 // num of char in each record or line
