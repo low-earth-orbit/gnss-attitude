adfb51e9e8f1bf993c2a41ae8f1caf3cd05ba1f2
Leo Hong <5917188+Low-Earth-Orbit@users.noreply.github.com>
2021-11-22 16:40:34 -0400

    Rename

--- run_simulation.sh
+++ run_simulation.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 echo "GNSS Single Antenna Attitude Determination"
+echo "Simulation Running"
 echo "Data Generation ..."
 SECONDS=0
 gcc -Wall simulation.c util.c struct.c -o simulation  -lgsl -lgslcblas -lm
-./simulation > input.txt
+./simulation
 echo "Completed in $SECONDS seconds"
 echo "Processing ..."
 SECONDS=0
 gcc -Wall antenna.c util.c struct.c snr.c -o antenna -lgsl -lgslcblas -lm
-./antenna > output.txt
+./antenna
 echo "Completed in $SECONDS seconds"
 echo "Exiting ..."
