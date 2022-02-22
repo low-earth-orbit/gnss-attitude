678ce8ae102e9cc2fb30fcb9d0ebaf4068674344
Leo Hong <5917188+Low-Earth-Orbit@users.noreply.github.com>
2022-02-22 17:32:38 -0400

    Update config.h

--- config.h
+++ config.h
@@ -2,30 +2,30 @@
 #define CONFIG_H
 
 /* Antenna parameters */
-#define ANT_SNR_STD_MIN 1	 // 1.02 //(1.02 + 1.52) / 2
-#define ANT_SNR_STD_MAX 5	 // 4.53 //(4.53 + 3.75) / 2
-#define ANT_SNR_RAW_MIN -100 // absolute min measured SNR
-#define ANT_SNR_RAW_MAX 200	 // absolute maximum measured SNR
+#define ANT_SNR_STD_MIN 1.27 // 1.02 //(1.02 + 1.52) / 2
+#define ANT_SNR_STD_MAX 4.14 // 4.53 //(4.53 + 3.75) / 2
+#define ANT_SNR_RAW_MIN 10	 // absolute min measured SNR
+#define ANT_SNR_RAW_MAX 60	 // absolute maximum measured SNR
 #define ANT_SNR_ADJ_MIN 42	 // average min adjusted SNR
 #define ANT_SNR_ADJ_MAX 50	 // 50 // average max adjusted SNR
 #define SAT_CUTOFF 0		 // satellite cutoff elevation angle
 
 /* antenna truth for statistics or simulation */
 #define TRUE_EL -60 // in degrees
-#define TRUE_AZ 180
+#define TRUE_AZ -180
 
 /* processing options */
 #define CONVERGENCE_CORRECTION true // elevation corrector to determined elevation angle
-#define OUTLIER_FACTOR 100			// sigma factor SNR outlier factor
+#define OUTLIER_FACTOR 3			// sigma factor SNR outlier factor
 
 /* simulation mode */
-#define SIMULATION true // simulation switch
-#define NUM_EPOCH 10000 // number of epochs to simulate
+#define SIMULATION false // simulation switch
+#define NUM_EPOCH 10000	 // number of epochs to simulate
 #define SIMULATE_SAT_VISIBLE true
 #define NUM_SAT_SPHERE 100 // number of GNSS satellites globally available
 #define NUM_SAT_VISIBLE 30 // number of satellite in the epoch to simulate
 #define SNR_A 10		   // quadratic mapping function SNR = -( SNR_A /8100) (off-boresight angle in degrees)^2 + SNR_C
-#define SNR_C 50		   // max SNR value
+#define SNR_C 100		   // max SNR value
 #define SNR_STD_MIN 0.5	   // minimum snr standard deviation i.e. at 0 deg off-boresight angle
 #define SNR_STD_MAX 0.5	   // max snr standard deviation i.e. at 90 deg off-boresight angle
 #define SKEW false		   // simulate multipath
